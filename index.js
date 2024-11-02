cc32fe6a9a70fd94eebf494f0f589ac9745081fd
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-11-02 11:05:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var diasem = 6;
+
+if(diasem == 1){
+    console.log('lunes')
+}else{
+    if(diasem == 2){
+        console.log('martes')
+    }else{
+        if(diasem == 3){
+            console.log('miercoles')
+        }else{
+            if(diasem == 4){
+                console.log('jueves')
+            }else{
+                if (diasem == 5) {
+                    console.log('viernes')
+                } else {
+                    if (diasem == 6) {
+                        console.log('sabado')
+                    } else {
+                        if (diasem == 7) {
+                            console.log('domingo')
+                        } else {
+                            console.log('escoga un nuemro valido del 1 al 7')
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
